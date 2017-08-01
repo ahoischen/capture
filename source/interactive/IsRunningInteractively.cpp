a10e8f55f7fc35c977e16a9d83c9913e98c50aad
Alexander Hoischen <alexander.hoischen@gmx.net>
2017-08-01 10:52:20 +0200

    Fix IsRunningInteractively for citra
    Citra's input detection caused random values to appear for hidKeysDown()
    calls. hidKeysHeld() produced similar unreliable results. My solution
    was to first hidScanInput() to clear randomness and then launch an
    aptMainLoop for 2 seconds. Within these two seconds the user has to
    press a button to launch interactively.
    
    Closes #16

--- source/interactive/IsRunningInteractively.cpp
+++ source/interactive/IsRunningInteractively.cpp
@@ -2,10 +2,25 @@
 
 #include "3ds.h"
 #include <cstdio>
+#include <iostream>
+#include <string>
 
 namespace capture::interactive {
     bool IsRunningInteractively() {
+        bool ButtonPressed{false};
+        std::cout << "Press any button to launch interactive mode." << std::endl;
+
+        // Citra is weird abouts it's inputs. This should clear the initial random value.
         hidScanInput();
-        return hidKeysDown() != 0;
+
+        // Citra only properly detects inputs in an aptMainLoop. The user has 120 frames(2 seconds)
+        // to initiate an interactive session.
+        for(int i = 0; i < 120 && aptMainLoop() && !ButtonPressed; ++i) {            
+            hidScanInput();
+            ButtonPressed = hidKeysDown() != 0;
+            gspWaitForVBlank();
+        }
+
+        return ButtonPressed;
     }
 }
